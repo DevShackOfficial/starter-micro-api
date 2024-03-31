0dea9622c19f558225f3c832da60568e970816ae
Dev Shack Official Account for Github <devshack.official@gmail.com>
2024-03-31 09:49:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function handle(){
   console.log("Handled bot");
   const bot = mf.createBot({host: process.env.HOST??"kaboom.pw", port: process.env.PORT??25565, username: process.env.USERNAME??"LoggerExe", version: "1.19.2"});
 
+  bot._client.on("connect", ()=> {console.log("Trying to connect")});
   bot.on("login", () => {bot.chat("E");console.log("Joined! Logging server");});
   bot.on("message", (msg)=> {console.log(msg.toAnsi());});
   bot.on("end", handle);
