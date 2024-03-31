93e2cd16babce640eeda6a74e06e6adcd9aa1f74
Dev Shack Official Account for Github <devshack.official@gmail.com>
2024-03-31 09:41:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const mf = require("mineflayer");
 
+function handle(){
+  const bot = mf.createBot({host: process.env.HOST, port: process.env.PORT, username: process.env.USERNAME, version: "1.19.2"});
+
+  bot.on("login", () => {bot.chat("E");console.log("Joined! Logging server");});
+  bot.on("message", (msg)=> {console.log(msg.toAnsi());});
+  bot.on("end", handle);
+}
+handle();
